f69007303e2f58b27c8482586171b47da0a2b890
JigraNoob <jeremy.b.richards@gmail.com>
2025-07-11 16:20:25 -0500

    Update glint_atlas.js

--- glint_atlas.js
+++ glint_atlas.js
@@ -1,1 +1,14 @@
+// In glint_atlas.js or a separate firebase-init.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+initializeApp(firebaseConfig);
 
